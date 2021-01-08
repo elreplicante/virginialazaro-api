f10a31acd85e7bcdeaec3ee03caa592a75d83494
Sergio Revilla <replymusic@gmail.com>
2021-01-08 20:10:14 +0100

    Fix dependencies

--- virginialazaro/settings.py
+++ virginialazaro/settings.py
@@ -1,8 +1,5 @@
 import os
 from pathlib import Path
-import django_heroku
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'a-secret-key')
@@ -119,11 +116,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 try:
+    import django_heroku
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
     sentry_sdk.init(
         dsn=os.environ.get('SENTRY_DSN'),
         integrations=[DjangoIntegration()],
         traces_sample_rate=1.0,
     )
+    django_heroku.settings(locals())
 except ModuleNotFoundError:
     pass
 
@@ -133,4 +134,4 @@ LIST_ARTICLES_BY_CATEGORY = {
     'interviews': 2,
 }
 
-django_heroku.settings(locals())
+
